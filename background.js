01f27a3aa09ddd2c19195f81b32fd7814ed0bfbd
Henrique Morbin <morbin_@hotmail.com>
2016-07-24 18:57:31 -0300

    fix text

--- background.js
+++ background.js
@@ -19,6 +19,4 @@ var addShipitButton = function() {
 
   $shipitButton.insertAfter($commentActions.find("button:first"));
 };
-
 $(document).ready(addShipitButton);
-$(document).mousemove(addShipitButton);
